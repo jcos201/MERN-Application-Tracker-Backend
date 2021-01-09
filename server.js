ff0ba47ade90b3c4944fa24e0e6b3e68bd2effad
Jorge L. Cossi <jcos201@gmail.com>
2021-01-09 15:37:25 -0500

    commits server.js changes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
+require('dotenv').config();
+
 app.use(logger('dev'));
-
 app.use(express.json());
-
+app.use(cors());
 
 const port = process.env.PORT || 3001;
 
