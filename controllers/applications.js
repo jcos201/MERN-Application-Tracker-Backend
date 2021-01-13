47ede5ebb7c921bc09f94616cffaabb663fffb30
Jorge L. Cossi <jcos201@gmail.com>
2021-01-13 18:36:48 -0500

    added applications controlleer

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,9 +1,31 @@
 const User = require('../models/user');
 
+module.exports = {
+    showAllAppListings,
+    addAppListing,
+    updateAppListing,
+    deleteAppListing,
+}
+
 async function showAllAppListings(req,res){
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+    } catch (error) {
+        res.status(400).json({err: 'bad request'});
+
+    }
 
 };
 
 async function addAppListing(req, res){
+
+};
+
+async function updateAppListing(req, res){
+
+};
+
+async function deleteAppListing(req, res){
 
 };
