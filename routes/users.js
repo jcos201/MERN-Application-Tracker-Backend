18835303c6ee7cc0a00a50f7de9c8fc375007270
Jorge L. Cossi <jcos201@gmail.com>
2021-01-13 21:51:11 -0500

    updated POST addapplication route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const usersCtrl = require('../controllers/users');
+const applicationsCtrl = require('../controllers/applications');
+
 
 router.post('/signup', usersCtrl.signup);
 router.post('/login', usersCtrl.login);
+
+router.post('/addapplication', applicationsCtrl.addAppListing);
+
 
 
 module.exports = router; 
