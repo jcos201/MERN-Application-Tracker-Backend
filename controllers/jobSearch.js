131e3b7728c9ed1bc3284c1d5e935eff80d8a681
Jorge L. Cossi <jcos201@gmail.com>
2021-01-15 06:26:22 -0500

    added addSearch function

--- controllers/jobSearch.js
+++ controllers/jobSearch.js
@@ -7,5 +7,14 @@ module.exports = {
 }
 
 async function addSearch(req, res){
-
+    try {
+        const user = await User.findOne({email: req.body.user.email});
+        if(!user) return res.status(401).json({err: 'bad credentials'});
+        user.savedJobSearches.push(req.body);
+        user.save();
+        const token = req.body.token;
+        res.json({token});
+    } catch (error) {
+        res.status(400).json({err:'bad request'});
+    }
 }
