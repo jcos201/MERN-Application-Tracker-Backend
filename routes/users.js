18786586ace6cb67ee11f395d66802fede95417e
Jorge L. Cossi <jcos201@gmail.com>
2021-01-14 20:02:39 -0500

    added job search route

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const usersCtrl = require('../controllers/users');
 const applicationsCtrl = require('../controllers/applications');
+const jobSearchCtrl = require('../controllers/jobSearch');
 
 
 router.post('/signup', usersCtrl.signup);
 router.post('/login', usersCtrl.login);
 
 router.post('/addapplication', applicationsCtrl.addAppListing);
+
+router.post('/addSearch', jobSearchCtrl.addSearch);
 
 
 
