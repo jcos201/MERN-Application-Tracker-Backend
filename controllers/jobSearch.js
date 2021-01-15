4a61e74882b3ebb9075013941531005f4e7999ef
Jorge L. Cossi <jcos201@gmail.com>
2021-01-14 20:03:16 -0500

    added jobSearch controller

---
+++ controllers/jobSearch.js
@@ -0,0 +1,11 @@
+const User = require('../models/user');
+const SECRET = process.env.SECRET;
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    addSearch,
+}
+
+async function addSearch(req, res){
+
+}
