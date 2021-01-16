b5fcc32bb05a98fb9936bd8d4bb1c3cf64b56896
Jorge L. Cossi <jcos201@gmail.com>
2021-01-16 10:11:02 -0500

    commented out console logs

--- controllers/applications.js
+++ controllers/applications.js
@@ -8,6 +8,17 @@ module.exports = {
 }
 
 async function showAllAppListings(req,res){
+    try {
+        //console.log('inside showAllAppListings')
+        //console.log(req.body);
+        const user = await User.findOne({ email: req.user.email });
+       // console.log('application array:')
+        //console.log(user.applications)
+        const applicationArray = user.applications;
+        res.json({ applicationArray })
+    } catch (error) {
+        res.status(400).json({err: 'bad request'})
+    }
 
 };
 
@@ -16,14 +27,15 @@ async function addAppListing(req, res){
     console.log('req.body')
     console.log(req.body)
     try {
-        const user = await User.findOne({ email: req.body.user.email });
+        const user = await User.findOne({ email: req.user.email });
         if(!user) return res.status(401).json({err: 'bad credentials'});
         user.applications.push(req.body);
-        user.save();
-        console.log('user')
-        console.log(user);
+        await user.save();
+        const applicationArray = user.applications;
+        //console.log('user applications')
+        //console.log(applicationArray);
         const token = req.body.token;
-        res.json({ token });
+        res.json({ applicationArray });
         
     } catch (error) {
         res.status(400).json({err: 'bad request'});
