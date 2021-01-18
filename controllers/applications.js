890e4f08671b33387e07a668340916aa259d5bfe
Jorge L. Cossi <jcos201@gmail.com>
2021-01-18 06:33:38 -0500

    added showOneListing function

--- controllers/applications.js
+++ controllers/applications.js
@@ -5,6 +5,7 @@ module.exports = {
     addAppListing,
     updateAppListing,
     deleteAppListing,
+    showOneListing,
 }
 
 async function showAllAppListings(req,res){
@@ -12,6 +13,8 @@ async function showAllAppListings(req,res){
         //console.log('inside showAllAppListings')
         //console.log(req.body);
         const user = await User.findOne({ email: req.user.email });
+        if(!user) return res.status(401).json({err: 'bad credentials'});
+
        // console.log('application array:')
         //console.log(user.applications)
         const applicationArray = user.applications;
@@ -34,7 +37,7 @@ async function addAppListing(req, res){
         const applicationArray = user.applications;
         //console.log('user applications')
         //console.log(applicationArray);
-        const token = req.body.token;
+       // const token = req.body.token;
         res.json({ applicationArray });
         
     } catch (error) {
@@ -55,6 +58,22 @@ async function deleteAppListing(req, res){
 
 };
 
-function createListing(user, listing) {
+async function showOneListing(req, res) {
+    console.log('inside show one listing');
+    try {
+        const user = await User.findOne({ email: req.user.email });
+        if(!user) return res.status(401).json({err: 'bad credentials'});
+        console.log('application id from req.body');
+        console.log(req.params.id);
+        const listing = await user.applications.find( application =>  application._id == req.params.id);
+        if(!listing) return res.status(401).json({err: 'bad application credentials'});
+        console.log(listing)
+        res.json({ listing });
+        
+    } catch (error) {
+        res.status(400).json({err: 'bad request'})
+
+    }
+
 
 }
