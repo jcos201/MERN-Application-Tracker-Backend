bc17fa390c2c30d3426f4588a487ea641f9c2d42
Jorge L. Cossi <jcos201@gmail.com>
2021-01-19 14:58:42 -0500

    add application listing deletion route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const usersCtrl = require('../controllers/users');
 const applicationsCtrl = require('../controllers/applications');
 const jobSearchCtrl = require('../controllers/jobSearch');
+const { application } = require('express');
 
 
 router.post('/signup', usersCtrl.signup);
@@ -10,7 +11,9 @@ router.post('/login', usersCtrl.login);
 router.post('/addapplication', applicationsCtrl.addAppListing);
 router.get('/applications' , applicationsCtrl.showAllAppListings);
 router.get('/applications/:id', applicationsCtrl.showOneListing);
-router.post('/applications/:id', applicationsCtrl.updateAppListing);
+router.put('/applications/:id', applicationsCtrl.updateAppListing);
+router.delete('delete/:id', applicationsCtrl.deleteAppListing);
+
 
 router.post('/addSearch', jobSearchCtrl.addSearch);
 
